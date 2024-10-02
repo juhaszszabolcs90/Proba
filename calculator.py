edc12c04d0b3102a14324439bd8c5f4789a5ca6c
Juhasz Szabolcs <juhaszszabolcs90>
2024-10-02 11:21:54 +0200

    Add calculator file

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_substraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
